51c6992218a2d62f34050204a46073600e62394b
unknown <penbora97@gmail.comgit config --global user.name penbora97-hash>
2026-08-11 16:02:37 +0700

    fix bootstrap cache path for vercel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// បង្កើត Temporary Storage Folders នៅលើ Vercel
+// បង្កើត Temporary Storage & Cache Folders ក្នុង /tmp
 $storagePaths = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache/data',
@@ -15,8 +15,12 @@ foreach ($storagePaths as $path) {
     }
 }
 
-// កំណត់ Storage Path និង View Compiled Path ទៅកាន់ /tmp
+// កំណត់ Path ទាំងអស់ឱ្យទៅប្រើប្រាស់ /tmp
 putenv('APP_STORAGE=/tmp/storage');
 putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
 
 require __DIR__ . '/../public/index.php';
