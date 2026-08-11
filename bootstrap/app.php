27b7d07acf9b8fbe09221e4c8d9f8973328b8289
unknown <penbora97@gmail.comgit config --global user.name penbora97-hash>
2026-08-11 16:10:47 +0700

    override bootstrap cache path in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,33 @@
 <?php
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
+    $paths = [
+        '/tmp/storage/framework/views',
+        '/tmp/storage/framework/cache/data',
+        '/tmp/storage/framework/sessions',
+        '/tmp/storage/logs',
+        '/tmp/bootstrap/cache',
+    ];
+    foreach ($paths as $path) {
+        if (!is_dir($path)) {
+            mkdir($path, 0755, true);
+        }
+    }
 
+    $_ENV['APP_STORAGE'] = '/tmp/storage';
+    $_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+    $_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+    $_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
+    $_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes.php';
+}
+
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+// ប្រសិនបើជា Vercel ត្រូវបង្ខំ useStoragePath ទៅ /tmp
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
+    $app->useStoragePath('/tmp/storage');
+}
 /*
 |--------------------------------------------------------------------------
 | Create The Application
